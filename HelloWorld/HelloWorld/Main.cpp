5b403061e5e86ad35bfeaf26faaa744e3a43c016
Ders52 <Houston9452@yahoo.com>
2014-01-14 12:12:15 -0600

    initial check in

---
+++ HelloWorld/HelloWorld/Main.cpp
@@ -0,0 +1,18 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Hello World" << endl;
+	}
+
+	cout << "Goodbye";
+
+	_getch();
+
+	return 1;
+}
