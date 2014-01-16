c1a7464faae2950ac75469ed486cc617b9983dc8
Ders52 <Houston9452@yahoo.com>
2014-01-16 15:14:58 -0600

    check in assignment 2

---
+++ Assign2_Cout/Assign2_Cout/Main.cpp
@@ -0,0 +1,23 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//output a line and start a new line	
+	cout << "Computers, computers everywhere" << endl;
+
+	//output a tabbed in line and start a new line	
+	cout << "\tas far as I can C." << endl; 
+	
+	//output a line and start a new line	
+	cout << "I really, really like these things." << endl;
+	
+	//output a tabbed in line and start a new line	
+	cout << "\tOh joy. Oh joy for me!";
+	
+	//wait to close te program until a key is pressed
+	_getch();
+	return 1;
+}
