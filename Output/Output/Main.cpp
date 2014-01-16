d5940f4ea2867ef7fa5bfe01874c7d2cca10c278
Ders52 <Houston9452@yahoo.com>
2014-01-16 11:21:54 -0600

    working input output check in

--- Output/Output/Main.cpp
+++ Output/Output/Main.cpp
@@ -5,9 +5,21 @@ using namespace std;
 
 int main()
 {
+	//declare a num variable
+	int num = 28;
 
-	cout << "working" << endl;
+	//ask for a number
+	cout << "Please enter a number" << endl;
 
+	//get a number from input
+	cin >> num;
+
+	//display number inputted
+	cout << "The number is: " << num << endl;
+
+	cout << "\tthis is some text" << endl;
+
+	//wait for key press to exit
 	_getch();
 	return 1;
 }
