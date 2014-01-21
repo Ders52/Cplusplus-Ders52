5a0cf9133886064123cfebe82c1ea73a1c16f6b6
Ders52 <Houston9452@yahoo.com>
2014-01-21 11:12:10 -0600

    initial check in datatypes

---
+++ ClassWorkDataTypes/ClassWorkDataTypes/Main.cpp
@@ -0,0 +1,19 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num = 5;
+
+	bool success = true;
+
+	cin >> num;
+
+	cout << num; 
+
+	_getch();
+
+	return 1;
+}
