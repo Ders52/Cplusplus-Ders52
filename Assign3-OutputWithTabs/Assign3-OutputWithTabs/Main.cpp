2e65023a6bb08ebbfe00e337f41c41d19efcc7c8
Ders52 <Houston9452@yahoo.com>
2014-01-21 11:48:23 -0600

    final Assign3

---
+++ Assign3-OutputWithTabs/Assign3-OutputWithTabs/Main.cpp
@@ -0,0 +1,16 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Part No.\t " << "  Price" << endl;
+	cout << "T1267\t\t   " << "$6.34 " << endl;
+	cout << "T1300\t\t   " << "$8.92 " << endl;
+	cout << "T2401\t\t  " << "$65.40 " <<endl;
+	cout << "T4482\t\t " << "$103.45 " << endl;
+
+	_getch();
+	return 1;
+}
