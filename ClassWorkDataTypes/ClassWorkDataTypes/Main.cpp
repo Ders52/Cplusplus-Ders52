d998d07c41899c5cfeed900b0fce746bb18fd750
Ders52 <Houston9452@yahoo.com>
2014-01-21 11:29:38 -0600

    output different variables

--- ClassWorkDataTypes/ClassWorkDataTypes/Main.cpp
+++ ClassWorkDataTypes/ClassWorkDataTypes/Main.cpp
@@ -1,17 +1,27 @@
 #include <conio.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	int num = 5;
-
+	int num = 0;
+	int num2 = 27;
 	bool success = true;
+	double miles = 0;
+	string hello = "hi there";
+	char c = 'c';
 
 	cin >> num;
+	cin >> miles;
 
-	cout << num; 
+	cout << "num = " << num << endl; 
+	cout << "success = " << success << endl;
+	cout << "miles = " << miles << endl; 
+	cout << hello << endl;
+	cout << "c = " << c << endl;
+	cout << "num + num2 = " << num + num2 << endl; 
 
 	_getch();
 
