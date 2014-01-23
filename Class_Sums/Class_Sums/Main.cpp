f88803f49507b09ab119029c457d4f84f7625378
Ders52 <Houston9452@yahoo.com>
2014-01-23 11:17:39 -0600

    End Class Sums

--- Class_Sums/Class_Sums/Main.cpp
+++ Class_Sums/Class_Sums/Main.cpp
@@ -1,11 +1,42 @@
 #include <conio.h>
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-	cout << "Hello";
+	double num1 = 1.37, num2 = 2, num3 = 3, 
+		   sum = 4,
+		   product = 0,
+		   avg = 0,
+		   exp = 0,
+		   modulus = 0; 
+
+	sum = num1 + num2 + num3;
+	avg = sum / 3;
+	product = num1 * num2;
+	exp = pow(2,4);
+	modulus = 5 % 2;
+
+
+	cout << "Number 1 = " << num1 << endl;
+	cout << "Number 2 = " << num2 << endl;
+	cout << "Number 3 = " << num3 << endl;
+	
+	//sets fixed decimal place
+	cout << setprecision(1) << fixed;
+
+	cout << "Product = " << product << endl;
+	cout << "Sum = " << sum << endl;
+	
+	//cout << setprecision(1) << fixed;
+	//turns off fixed decimall place
+	//cout << setprecision(1) << resetiosflags(ios::fixed);
+
+	cout << "Avg = " << avg << endl;
+	cout << "Exp = " << exp << endl; 
+	cout << "Modulus = " << modulus << endl; 
+
 	_getch();
 	return 1;
 }
