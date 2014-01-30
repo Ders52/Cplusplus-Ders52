b62c38fbae01f8f40d31f4233fb840c22fdb9b66
Ders52 <Houston9452@yahoo.com>
2014-01-30 10:41:29 -0600

    assign1-Pseudocode2 initial/final check in

---
+++ Assign1_Psuedocode2/Assign1_Psuedocode2/Main.cpp
@@ -0,0 +1,22 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	//declare variables/constants for change and denominations
+
+	//Ask for the amount of purchase	
+
+	//get the purchase amount
+
+	//subtract purchase amount from $10 to calculate Change amount
+
+	//determine largest denomination of currency that can be given
+	//Subtract the denomination from change and repeat the process until change left to give is $0
+
+	//output the denominations of the change given
+
+}
