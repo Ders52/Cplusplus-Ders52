dd2d00418baab96efed56ea3252194fdd20aea47
Ders52 <Houston9452@yahoo.com>
2014-02-04 12:09:29 -0600

    final assign6 with comments

--- Assignment6-TempConversion/Assignment6-TempConversion/Main.cpp
+++ Assignment6-TempConversion/Assignment6-TempConversion/Main.cpp
@@ -6,21 +6,25 @@ using namespace std;
 
 int main()
 {
+	//declare variables
 	double tempFar;
 	double tempCels;
 
+	//ask for input
 	cout << "Please enter a temperature in Celsius: ";
 
+	//get input for starting temp
 	cin >> tempCels;
 
+	//convert input to farenheit
 	tempFar = 1.8 * tempCels + 32;
 
+	//set number of decimal places
 	cout << fixed << setprecision(2);
 
+	//display converted temp
 	cout << endl << tempCels << " degrees Celsius converted into Farenheit is: " << tempFar;
-
-
-
+	
 
 	_getch();
 	return 1;
